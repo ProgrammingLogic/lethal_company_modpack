8f9d1e2b27ddeb87236246974d9d9c508c219c26
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-09-04 20:00:54 -0500

    feat: automate more of update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,12 @@
+ProfileName="Modded Lethal Company"
+ProfileName=(echo "$ProfileName" | sed -r "s/\ /\\\ ")
+
+cp -rvf "../exports/$ProfileName_*/" ../
+
 git add ../mods.yml
 git add ../export.r2x
 git add ../config
 
 git commit --verbose
+
+rm -rf ../exports/*
